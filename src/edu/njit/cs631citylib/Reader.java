3a8a96c105755245345e4c248f6e835fa616de02
xin yin <xin.yin@uconn.edu>
2016-12-11 16:01:17 -0500

    Reader.java

--- src/edu/njit/cs631citylib/Reader.java
+++ src/edu/njit/cs631citylib/Reader.java
@@ -1,81 +1,112 @@
 package edu.njit.cs631citylib;
 
+import java.awt.*;
+import java.awt.event.*;
+
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.*;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+import javax.swing.JList;
+import javax.swing.JSpinner;
 
 public class Reader extends JDialog {
 
 	private JTextField txtDocSearch;
-	private JRadioButton radioButton_1;
-	private JRadioButton rdbtnPublisher;
+	private JRadioButton radioButtonTitle;
+	private JRadioButton radioButtonPublisher;
+	private JLabel stausLabel;
 
 
 	/**
 	 * Create the frame.
 	 */
 	public Reader() {
-		
+		getContentPane().setBackground(Color.WHITE);
+		//Connect to Database
 		DBManager m = DBManager.getInstance();
-		if (!m.connect()) {
-			JOptionPane.showMessageDialog(null, "Could not connect to database");
-			System.exit(1);
-		}
-
-		setBounds(100, 100, 450, 300);
+	
+		setBounds(100, 100, 539, 469);
 		getContentPane().setLayout(null);
 		
 		txtDocSearch = new JTextField();
-		txtDocSearch.setBounds(28, 47, 300, 26);
+		txtDocSearch.setBounds(28, 47, 355, 26);
 		getContentPane().add(txtDocSearch);
 		txtDocSearch.setColumns(10);
 		
-		JRadioButton radioButton = new JRadioButton("Decoument Id ");
-		radioButton.setBounds(28, 75, 124, 23);
-		getContentPane().add(radioButton);
+		JLabel lblNewLabel = new JLabel("City Library");
+		lblNewLabel.setForeground(Color.BLACK);
+		lblNewLabel.setBounds(184, 20, 81, 26);
+		getContentPane().add(lblNewLabel);
 		
-		radioButton_1 = new JRadioButton("Title");
-		radioButton_1.setBounds(148, 75, 68, 23);
-		getContentPane().add(radioButton_1);
+		JLabel lblReadId = new JLabel("READ ID");
+		lblReadId.setBounds(16, 6, 61, 16);
+		getContentPane().add(lblReadId);
 		
-		rdbtnPublisher = new JRadioButton("Publisher");
-		rdbtnPublisher.setBounds(209, 75, 95, 23);
-		getContentPane().add(rdbtnPublisher);
+		//Radio Button
+		JRadioButton radioButtonDocId = new JRadioButton("Decoument Id ");
+		radioButtonDocId.setSelected(true);
+		radioButtonDocId.setBounds(28, 75, 124, 23);
+		getContentPane().add(radioButtonDocId);
 		
+		radioButtonTitle = new JRadioButton("Title");
+		radioButtonTitle.setBounds(148, 75, 68, 23);
+		getContentPane().add(radioButtonTitle);
+		
+		radioButtonPublisher = new JRadioButton("Publisher");
+		radioButtonPublisher.setBounds(209, 75, 95, 23);
+		getContentPane().add(radioButtonPublisher);
+		
+		ButtonGroup bG = new ButtonGroup();
+		bG.add(radioButtonDocId);
+		bG.add(radioButtonTitle);
+		bG.add(radioButtonPublisher);
+		
+		
+	
+		//Search Button
 		JButton btnSearch = new JButton("Search");
 		btnSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 				if (txtDocSearch.getText().length() <= 0) {
-					JOptionPane.showMessageDialog(null, "Please type card number");
+					JOptionPane.showMessageDialog(null, "Invaid");
 					return;
 				}
 				
+				
 				ArrayList<ArrayList<Object>> result = m.execQuery("SELECT `id`, `title` FROM `DOCUMENT` WHERE `id` = " + txtDocSearch.getText() + ";");
 				if (result == null || result.size() != 1) {
-					JOptionPane.showMessageDialog(null, "Invalid card number");
+					JOptionPane.showMessageDialog(null, "Invaid Value");
 				} else {
 					System.out.println("valid");
+					SearchResult dialog = new SearchResult();
+					dialog.searchKeyword = txtDocSearch.getText();
+					if (radioButtonTitle.isSelected()) {
+						dialog.searchType = dialog.SEARCH_TYPE_TITLE;
+					} else if (radioButtonPublisher.isSelected()) {
+						dialog.searchType = dialog.SEARCH_TYPE_PUBLISHER ;
+						
+					} else {
+						dialog.searchType = dialog.SEARCH_TYPE_ID;
+					}
+					dialog.setModal(true);
+					dialog.setVisible(true);
 				}
 				
 			}
 		}
 	);
-		btnSearch.setBounds(327, 47, 117, 29);
+		btnSearch.setBounds(395, 47, 117, 29);
 		getContentPane().add(btnSearch);
-		
-		JLabel lblNewLabel = new JLabel("City Library");
-		lblNewLabel.setForeground(Color.BLACK);
-		lblNewLabel.setBounds(180, 19, 81, 26);
-		getContentPane().add(lblNewLabel);
 		
 	}
 }
