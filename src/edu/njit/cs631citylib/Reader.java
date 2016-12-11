5f6dd8e2f33953d856728d8b59f998a81a0f5003
xin yin <xin.yin@uconn.edu>
2016-12-11 17:21:34 -0500

    Reader.java, Reserve.java

--- src/edu/njit/cs631citylib/Reader.java
+++ src/edu/njit/cs631citylib/Reader.java
@@ -34,17 +34,18 @@ public class Reader extends JDialog {
 		//Connect to Database
 		DBManager m = DBManager.getInstance();
 	
-		setBounds(100, 100, 539, 469);
+		setBounds(100, 100, 542, 286);
 		getContentPane().setLayout(null);
 		
 		txtDocSearch = new JTextField();
-		txtDocSearch.setBounds(28, 47, 355, 26);
+		txtDocSearch.setBounds(28, 70, 355, 26);
 		getContentPane().add(txtDocSearch);
 		txtDocSearch.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("City Library");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 		lblNewLabel.setForeground(Color.BLACK);
-		lblNewLabel.setBounds(184, 20, 81, 26);
+		lblNewLabel.setBounds(202, 24, 181, 50);
 		getContentPane().add(lblNewLabel);
 		
 		JLabel lblReadId = new JLabel("READ ID");
@@ -52,17 +53,17 @@ public class Reader extends JDialog {
 		getContentPane().add(lblReadId);
 		
 		//Radio Button
-		JRadioButton radioButtonDocId = new JRadioButton("Decoument Id ");
+		JRadioButton radioButtonDocId = new JRadioButton("Decoument Id");
 		radioButtonDocId.setSelected(true);
-		radioButtonDocId.setBounds(28, 75, 124, 23);
+		radioButtonDocId.setBounds(38, 108, 124, 23);
 		getContentPane().add(radioButtonDocId);
 		
 		radioButtonTitle = new JRadioButton("Title");
-		radioButtonTitle.setBounds(148, 75, 68, 23);
+		radioButtonTitle.setBounds(175, 108, 68, 23);
 		getContentPane().add(radioButtonTitle);
 		
 		radioButtonPublisher = new JRadioButton("Publisher");
-		radioButtonPublisher.setBounds(209, 75, 95, 23);
+		radioButtonPublisher.setBounds(255, 108, 95, 23);
 		getContentPane().add(radioButtonPublisher);
 		
 		ButtonGroup bG = new ButtonGroup();
@@ -78,35 +79,73 @@ public class Reader extends JDialog {
 			public void actionPerformed(ActionEvent e) {
 				
 				if (txtDocSearch.getText().length() <= 0) {
-					JOptionPane.showMessageDialog(null, "Invaid");
+					JOptionPane.showMessageDialog(null, "Enter a valid value");
 					return;
 				}
-				
-				
-				ArrayList<ArrayList<Object>> result = m.execQuery("SELECT `id`, `title` FROM `DOCUMENT` WHERE `id` = " + txtDocSearch.getText() + ";");
-				if (result == null || result.size() != 1) {
-					JOptionPane.showMessageDialog(null, "Invaid Value");
+
+				int searchType;
+				if (radioButtonTitle.isSelected()) {
+					searchType = SearchResult.SEARCH_TYPE_TITLE;
+				} else if (radioButtonPublisher.isSelected()) {
+					searchType = SearchResult.SEARCH_TYPE_PUBLISHER ;
 				} else {
-					System.out.println("valid");
-					SearchResult dialog = new SearchResult();
-					dialog.searchKeyword = txtDocSearch.getText();
-					if (radioButtonTitle.isSelected()) {
-						dialog.searchType = dialog.SEARCH_TYPE_TITLE;
-					} else if (radioButtonPublisher.isSelected()) {
-						dialog.searchType = dialog.SEARCH_TYPE_PUBLISHER ;
-						
-					} else {
-						dialog.searchType = dialog.SEARCH_TYPE_ID;
-					}
-					dialog.setModal(true);
-					dialog.setVisible(true);
+					searchType = SearchResult.SEARCH_TYPE_ID;
 				}
-				
+				SearchResult dialog = new SearchResult(txtDocSearch.getText(), searchType);
+				dialog.setModal(true);
+				dialog.setVisible(true);
 			}
-		}
-	);
-		btnSearch.setBounds(395, 47, 117, 29);
+		});
+
+		btnSearch.setBounds(395, 70, 117, 29);
 		getContentPane().add(btnSearch);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(46, 197, 1, 12);
+		getContentPane().add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(6, 197, 536, -60);
+		getContentPane().add(separator_1);
+		
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setBounds(0, 207, 542, 16);
+		getContentPane().add(separator_2);
+		
+		JLabel lblReaderProfile = new JLabel("Reader Profile");
+		lblReaderProfile.setBounds(28, 226, 86, 16);
+		getContentPane().add(lblReaderProfile);
+		
+		JButton btnNewButtonBorrow = new JButton("Borrow");
+		btnNewButtonBorrow.setBounds(142, 221, 87, 29);
+		getContentPane().add(btnNewButtonBorrow);
+		
+		JButton btnNewButtonReserve = new JButton("Reserve");
+		btnNewButtonReserve.setBounds(241, 221, 92, 29);
+		getContentPane().add(btnNewButtonReserve);
+		
+		JButton btnNewButtonHistory = new JButton("History");
+		btnNewButtonHistory.setBounds(345, 221, 90, 29);
+		getContentPane().add(btnNewButtonHistory);
+		
+		btnNewButtonHistory.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Reserve dialog = new Reserve();
+				dialog.setModal(true);
+				dialog.setVisible(true);
+			}
+		});
+		btnNewButtonReserve.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Reserve dialog = new Reserve();
+				dialog.setModal(true);
+				dialog.setVisible(true);
+			}
+		});
+		btnNewButtonBorrow.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		
 	}
 }
---
+++ src/edu/njit/cs631citylib/Reserve.java
@@ -0,0 +1,53 @@
+package edu.njit.cs631citylib;
+
+import java.awt.BorderLayout;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+
+public class Reserve extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			Reserve dialog = new Reserve();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public Reserve() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBackground(Color.WHITE);
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+	}
+
+}
