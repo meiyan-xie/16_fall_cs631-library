363b803b814a13ce14fa0955e9f304174e579a27
xin yin <xin.yin@uconn.edu>
2016-12-11 14:13:20 -0500

    New window

---
+++ src/edu/njit/cs631citylib/Reader.java
@@ -0,0 +1,54 @@
+package edu.njit.cs631citylib;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+
+public class Reader extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Reader frame = new Reader();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Reader() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(76, 47, 159, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JRadioButton radioButton = new JRadioButton(" ");
+		radioButton.setBounds(72, 77, 63, 23);
+		contentPane.add(radioButton);
+	}
+}
