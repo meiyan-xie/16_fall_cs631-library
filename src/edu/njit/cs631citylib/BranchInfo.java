68c50c27b703358fbbac7d320cea3f8233554529
sangatvaidya <sangatsvaidya@yahoo.co.in>
2016-12-13 17:02:45 -0500

    report generating classes

---
+++ src/edu/njit/cs631citylib/BranchInfo.java
@@ -0,0 +1,95 @@
+package edu.njit.cs631citylib;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+public class BranchInfo extends JDialog{
+	
+	private final JPanel contentPanel = new JPanel();
+	private JTable tableBranchInfo;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			DBManager m = DBManager.getInstance();
+			m.connect();
+			Borrow dialog = new Borrow("1");
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public BranchInfo() {
+		
+		DBManager m = DBManager.getInstance();
+		
+		String[] columnNames = {"NAME","LOCATION"};
+		ArrayList<ArrayList<Object>> result = new ArrayList<ArrayList<Object>>();
+		
+		result = m.execQuery("SELECT `LNAME`, `LLOCATION` FROM `BRANCH`;");
+
+		Object[][] array = new Object[result.size()][];
+		for (int i = 0; i < result.size(); i++) {
+			ArrayList<Object> row = result.get(i);
+			/*Timestamp a = (Timestamp)row.get(5);
+			Timestamp b = (Timestamp)row.get(6);
+			long diff = 0;
+			if (b == null){
+				long b2 = System.currentTimeMillis();
+				diff = b2 - a.getTime();
+			
+			}
+			else{
+				diff =  b.getTime() - a.getTime();
+				}
+			double diff1 = (double)diff / 86400000;
+			if (diff1 > 20){
+				diff1= diff1 - 20;
+				double diff2 = Math.ceil(diff1);
+				row.add(20*diff2);
+			}
+			else{
+				row.add(0.0);
+			}*/
+			array[i] = row.toArray();
+		}
+
+		//if (borrowResult == null || borrowResult.size() <= 0){
+			//JOptionPane.showMessageDialog(null, "No Reserved Documents");
+		//}
+
+		DefaultTableModel tm = new DefaultTableModel(array, columnNames);
+		
+		setBounds(100, 100, 1358, 610);
+		getContentPane().setLayout(null);
+		contentPanel.setBounds(0, 0, 1352, 582);
+		contentPanel.setBackground(Color.WHITE);
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel);
+		contentPanel.setLayout(null);
+	
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(26, 51, 1308, 480);
+		contentPanel.add(scrollPane);
+	
+		tableBranchInfo = new JTable();
+		scrollPane.setViewportView(tableBranchInfo);
+		tableBranchInfo.setModel(tm);
+
+	}
+
+}
--- src/edu/njit/cs631citylib/GenerateReport.java
+++ src/edu/njit/cs631citylib/GenerateReport.java
@@ -20,4 +20,79 @@ public class GenerateReport extends JDialog{
 		}
 	}
 
+	
+public GenerateReport() {
+		
+		
+        
+		
+        getContentPane().setBackground(Color.WHITE);
+		//Connect to Database
+		DBManager m = DBManager.getInstance();
+		m.connect();
+	
+		setBounds(100, 100, 500, 550);
+		getContentPane().setLayout(null);
+		
+		JLabel lblGenerateReport = new JLabel("Generate Reports");
+		lblGenerateReport.setBounds(38, 14, 200, 31);
+		getContentPane().add(lblGenerateReport);
+		
+		JButton btnBranchInfo = new JButton("Print Branch Info");
+		btnBranchInfo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				BranchInfo dialog = new BranchInfo();
+				dialog.setModal(true);
+				dialog.setVisible(true);
+				}
+		});
+		btnBranchInfo.setBounds(38, 64, 200, 29);
+		getContentPane().add(btnBranchInfo);
+			
+	JButton btnTopR = new JButton("Print top 10 Readers");
+	btnTopR.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					TopR dialog = new TopR();
+					dialog.setModal(true);
+					dialog.setVisible(true);
+					}	
+    });	
+	btnTopR.setBounds(38, 114, 200, 29);
+	getContentPane().add(btnTopR);
+    
+    JButton btnTopB = new JButton("Print top 10 borrowed books");
+    btnTopB.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					TopB dialog = new TopB();
+					dialog.setModal(true);
+					dialog.setVisible(true);
+					}	
+    });	
+    btnTopB.setBounds(38, 164, 200, 29);
+	getContentPane().add(btnTopB);
+	
+	JButton btnTopP = new JButton("Print top 10 popular books");
+	btnTopP.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					TopP dialog = new TopP();
+					dialog.setModal(true);
+					dialog.setVisible(true);
+					}	
+    });	
+	btnTopP.setBounds(38, 214, 200, 29);
+	getContentPane().add(btnTopP);
+	
+	JButton btnAvFn = new JButton("Print Average Fine per user");
+	btnAvFn.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					AvFn dialog = new AvFn();
+					dialog.setModal(true);
+					dialog.setVisible(true);
+					}	
+    });	
+	btnAvFn.setBounds(38, 264, 200, 29);
+	getContentPane().add(btnAvFn);
+		
+}
+
 }
